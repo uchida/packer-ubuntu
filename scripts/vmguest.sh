21587d0990aa8eeb12c9164b0c870691dcacc9af
Akihiro Uchida <uchida@turbare.net>
2015-06-20 11:20:15 +0900

    use wildcard to match virtualbox-iso and vmware-vmx

--- scripts/vmguest.sh
+++ scripts/vmguest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 case "$PACKER_BUILDER_TYPE" in
-  virtualbox-iso)
+  virtualbox-*)
     apt-get install -y build-essential "linux-headers-$(uname -r)"
     mount -o loop VBoxGuestAdditions.iso /mnt
     /mnt/VBoxLinuxAdditions.run || :
     umount /mnt
     rm -f VBoxGuestAdditions.iso
     ;;
-  vmware-iso)
+  vmware-*)
     apt-get install -y open-vm-tools
     ;;
 esac
