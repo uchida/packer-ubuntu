f8e25032540daf82de8596d8bccb1be2520176c1
pwasiewi <piotrwasiewiczeu@gmail.com>
2017-04-13 10:34:30 +0200

    Box załadował się na Atlas, ale ma jeszcze duży rozmiar

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -14,4 +14,4 @@ wget 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O ~vagr
 chmod 0600 ~vagrant/.ssh/authorized_keys
 chown -R vagrant:sudo ~vagrant
 
-passwd -l root
+echo root:vagrantkorzen | chpasswd
